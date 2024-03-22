e07c6c427230b3c4c6b2f60f9a6c1c3b39876616
UdayReddy4 <udayreddykr8339@gmail.com>
2024-03-22 12:44:37 +0530

    first commit

---
+++ 1_Node Module System/osModule.js
@@ -0,0 +1,9 @@
+const os=require('os')
+
+console.log(os.arch());
+
+console.log(os.platform());
+
+console.log(os.cpus());
+
+// THERE ARE so many methods like : cpus(),totalmem(),freemem(),uptime(),type()
---
+++ 1_Node Module System/path.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+
+let ext=path.extname('C:\\Users\\Admin\\Desktop\\node\\1_Node Module System\\f1path.txt')
+//          .basename('C:\Users\Admin\Desktop\node\1_Node Module System\f1path.txt')
+//   console.log(path.basename);
+console.log(ext);
+console.log(__dirname);
+console.log(__filename);
