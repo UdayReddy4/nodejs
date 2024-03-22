7ff1ea26a7cbb5818a60dd425eed265f0405df60
UdayReddy4 <udayreddykr8339@gmail.com>
2024-03-22 12:01:35 +0530

    childprocess

---
+++ 1_Node Module System/childProcess.js
@@ -0,0 +1,6 @@
+//child process is anode module used to create sub process within a script
+//you can perform different tasks with your script by just using some methods
+
+const cp =require('child_process')
+
+cp.execSync('calc')
