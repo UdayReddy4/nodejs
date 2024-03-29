5ebcaa5ddf4965992c52334d36eb5daf621fdc75
UdayReddy4 <udayreddykr8339@gmail.com>
2024-03-29 17:25:25 +0530

    first commit

--- 4_mongoDb/index.js
+++ 4_mongoDb/index.js
@@ -11,20 +11,79 @@ const courseSchema= new mongoose.Schema({
     name : String,
     creator : String,
     publishedDate : {type:Date ,default:Date.now},
-    isPublished : Boolean
+    isPublished : Boolean,
+    rating : Number
 })
 
 const Course = mongoose.model('Course',courseSchema)
 
 async function createCourse(){
     const course = new Course({
-        name : 'javaScript',
-        creator :'mrinal',
-        isPublished : true
-    })
+        name : 'c',
+        creator :'mal',
+        isPublished : true,
+        rating : 4.6
+    }) //creating data
     
     const result = await course.save()
     console.log(result);
+
+    
+}
+//createCourse()
+
+//eq(equal)
+//gt(greater than)
+//gte (greater than and equal to)
+//lt(less than)
+//lte(less than and equal to)
+
+//in
+//not in
+
+
+/* Logical Operators
+ or
+ and
+ */
+
+
+
+// async function getCourses(){
+// const courses=  await Course.find({rating : {$in :[4.4,4.5,4.6]}}).select({name :1})
+// .or([{creator :'mrina'},{rating :4}])
+
+// console.log(courses);
+
+// } //reading data
+
+// getCourses()
+
+
+
+async function updateCourse(id){
+let course =await Course.findById(id)
+if(!course) return;
+
+course.name ='c++'
+
+course.creator ='ila'
+
+const updateCourse =await course.save()
+console.log(updateCourse);
+
+
+} //updating
+
+updateCourse('6605539397c3d48a00cbffda')
+
+// deleting
+
+async function deleteCourse(id){
+    let course = await Course.findByIdAndDelete(id)
+    console.log(course);
 }
 
-createCourse()
+deleteCourse('6605539397c3d48a00cbffda')
+
+
